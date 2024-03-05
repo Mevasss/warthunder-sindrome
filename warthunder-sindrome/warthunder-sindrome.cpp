8079b94f2cea37176d0eedfdbd823df458cf2729
Mevas <Mevas@HOME-PC>
2024-03-05 13:18:30 +0300

    small fix

--- warthunder-sindrome/warthunder-sindrome.cpp
+++ warthunder-sindrome/warthunder-sindrome.cpp
@@ -19,12 +19,12 @@ int main()
         wt->c_hud->aircraft_selection(true);
         wt->c_hud->can_select_unit(true);
         wt->c_hud->ground_to_air_prediction(true);
-
+       
         for (auto unit : wt->unit_list) {
             if (unit.team_num() != wt->local_unit->team_num() ) 
                 std::cout << unit.player().name() << " " << unit.unit_info().tank_info().tank_name() << std::endl;
             //have troubles to cout russian names to console, but it is normal string -- test to file
-            std::cout << unit.player().name() << " " << unit.unit_info().tank_info().tank_name() << std::endl;
+            file << unit.player().name() << " " << unit.unit_info().tank_info().tank_name() << std::endl;
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
